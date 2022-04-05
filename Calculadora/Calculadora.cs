9790e4d0a02683e521fa030a0da9f85a926673a1
Andy-dev1 <andre.andre.jazz@gmail.com>
2022-04-04 22:28:41 -0300

    first commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Calculadora
+    {
+        public double Soma(double a, double b)
+        {
+            return a + b;
+        }
+        public double Divisao(double a, double b)
+        {
+            double c = 0.0;
+            c =  a / b;
+            return c;
+        }
+        public double Multiplicacao(double a, double b)
+        {
+            double c = 0.0;
+            c =  a * b;
+            return c;
+        }
+        public double Subtracao(double a, double b)
+        {
+            double c = 0.0;
+            c =  a - b;
+            return c;
+        }
+    }
+}
